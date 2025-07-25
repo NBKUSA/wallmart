5011f951873fb0763ddab6867f3d0ddd93891441
Lloydsgit <tejasreddynukella@gmail.com>
2025-07-25 14:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ PASSWORD_FILE = "password.json"
 # Ensure password file exists
 if not os.path.exists(PASSWORD_FILE):
     with open(PASSWORD_FILE, "w") as f:
-        hashed = hashlib.sha256("Br_3339".encode()).hexdigest()
+        hashed = hashlib.sha256("admin123".encode()).hexdigest()
         json.dump({"password": hashed}, f)
 
 def check_password(raw):
